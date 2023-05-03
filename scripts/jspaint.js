9eec6c6b4d5293ec303f39ce4c6d10bfda3b3295
asilio <94017839+cwpersonrennell@users.noreply.github.com>
2023-05-03 08:23:40 -0700

    Update jspaint.js

--- scripts/jspaint.js
+++ scripts/jspaint.js
@@ -1,3 +1,13 @@
-const canvas = document.getElementById("jspaint");
+function main(canvasId){
+	const canvas = document.getElementById(canvasId);
+	const ctx = canvas.getContext('2d');
+	console.log(canvas);
 
-console.log(canvas);
+	let isPainting = false;
+	let lineWidth = 10;\
+
+	
+
+}
+
+main("jspaint");
