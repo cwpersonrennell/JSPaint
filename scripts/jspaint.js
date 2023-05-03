1cb91701762e6d948a41c20ea3c1e57081d5c2ab
asilio <94017839+cwpersonrennell@users.noreply.github.com>
2023-05-03 09:49:40 -0700

    Update jspaint.js

--- scripts/jspaint.js
+++ scripts/jspaint.js
@@ -12,7 +12,13 @@ class JSPaint{
 		this.canvas.addEventListener("pointerdown",this.startPainting.bind(this));
 		this.canvas.addEventListener("pointerup",this.stopPainting.bind(this));
 
+		this.canvas.addEventListener("keyup",this.handleKeyEvent.bind(this))
+
 		}
+
+	handleKeyEvent(e){
+		console.log(e.keyCode);
+	}
 
 	penPosition(e){
 		this.x = e.clientX-this.canvas.offsetLeft;
