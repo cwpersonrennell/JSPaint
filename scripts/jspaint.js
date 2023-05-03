aea12b4f85eed16da4d083bb20e513b69beaa887
asilio <94017839+cwpersonrennell@users.noreply.github.com>
2023-05-03 08:41:23 -0700

    here we go

--- scripts/jspaint.js
+++ scripts/jspaint.js
@@ -1,13 +1,58 @@
-function main(canvasId){
-	const canvas = document.getElementById(canvasId);
-	const ctx = canvas.getContext('2d');
-	console.log(canvas);
+class JSPaint{
+	constructor(canvasID){
+		this.canvas= document.getElementById(canvasID);
+		this.ctx = canvas.getContext('2d');
+		this.isPainting=false;
+		this.lineWidth=10;
 
-	let isPainting = false;
-	let lineWidth = 10;\
+		this.x = 0;
+		this.y = 0;
 
-	
+		this.canvas.addEventListener("pointermove",penPosition);
+		this.canvas.addEventListener("pointerdown",this.startPainting);
+		this.canvas.addEventListener("pointerup",this.stopPainting);
 
+	}
+
+	penPosition(e){
+		this.x = e.clientX-this.canvas.offsetLeft;
+		this.y = e.clientY-this.canvas.offsetTop;
+	}
+
+	startPainting(e){
+		this.isPainting=true;
+		this.ctx.beginPath();
+		this.ctx.moveTo(this.x,this.y);
+	}
+
+	stopPainting(e){
+		this.isPainting=false;
+	}
+
+	update(){
+		if(!this.isPainting) return;
+		this.draw();
+	}
+
+	draw(){
+		this.ctx.lineWidth=this.lineWidth;
+		this.ctx.lineCap='round';
+		this.ctx.lineTo(this.x,this.y);
+		this.ctx.stroke();
+	}
 }
 
+
+
+function main(canvasId){
+	const paintApp = new JSPaint(canvasID);
+	
+	function JSPaintLoop(){
+		paintApp.update();
+		requestAnimationFrame(JSPaintLoop);
+	}
+	JSPaintLoop();
+}
+
+//Kick off the loop
 main("jspaint");
