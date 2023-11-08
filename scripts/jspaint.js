b05f3b73fd0d46b10406046f060756ab6e9938ff
cwpersonrennell <94017839+cwpersonrennell@users.noreply.github.com>
2023-11-07 20:21:46 -0700

    first pull

--- scripts/jspaint.js
+++ scripts/jspaint.js
@@ -1,18 +1,248 @@
+function debug(str){
+	console.log(`:\t${str}`);
+}
+const Instruction_Set=[
+	'STROKE', 'FILL','STROKESTYLE','FILLSTYLE','BEGINPATH',
+	'CLOSEPATH','MOVETO','LINETO',
+	'INTEGER','FLOAT','STRING',
+	'LINEWIDTH', 'LINECAP', 'LINEJOIN'
+	];
+
+const INSTRUCTIONS={};
+
+let bytecode = 0x00;
+for(let Instruction of Instruction_Set){
+	INSTRUCTIONS[Instruction]=++bytecode;
+}
+
+//0 Arguments
+const BEGINPATH = INSTRUCTIONS['BEGINPATH'];
+const STROKE = INSTRUCTIONS['STROKE'];
+const CLOSEPATH = INSTRUCTIONS['CLOSEPATH'];
+const FILL = INSTRUCTIONS['FILL'];
+
+//1 Argument
+	//Literals
+const FLOAT = INSTRUCTIONS['FLOAT'];
+const INTEGER = INSTRUCTIONS['INTEGER'];
+const STRING=INSTRUCTIONS['STRING'];
+	//Line parameters
+const LINEWIDTH=INSTRUCTIONS['LINEWIDTH'];
+const LINECAP=INSTRUCTIONS['LINECAP'];
+const LINEJOIN=INSTRUCTIONS['LINEJOIN'];
+
+//2 Arguments
+const LINETO = INSTRUCTIONS['LINETO'];
+const MOVETO = INSTRUCTIONS['MOVETO'];
+
+//4 Arguments
+const STROKESTYLE=INSTRUCTIONS['STROKESTYLE'];
+const FILLSTYLE=INSTRUCTIONS['FILLSTYLE'];
+
+// 0 argument instructions
+function instBEGINPATH(arr){
+	arr.push(BEGINPATH);
+	return arr;
+}
+function instSTROKE(arr){
+	arr.push(STROKE);
+	return arr;
+}
+function instCLOSEPATH(arr){
+	arr.push(CLOSEPATH);
+	return arr;
+}
+function instFILL(arr){
+	arr.push(FILL);
+	return arr;
+}
+//Literal Instructions
+function instINTEGER(arr,val){
+	return arr.concat([INTEGER,val]);
+}
+function instFLOAT(arr,val){
+	return arr.concat([FLOAT,val]);
+}
+function instSTRING(arr,val){
+	return arr.concat([STRING,val]);
+}
+//Convenience Functions for building Bytecode Array for instructions with parameters:
+function instLINEWIDTH(arr,val){
+	return arr.concat([FLOAT, val, LINEWIDTH]);
+}
+function instLINECAP(arr,val){
+	return arr.concat([STRING,val,LINECAP]);
+}
+function instLINEJOIN(arr,val){
+	return arr.concat([STRING,val,LINEJOIN]);
+}
+function instLINETO(arr,x,y){
+	return arr.concat([FLOAT, x, FLOAT, y, LINETO]);
+}
+function instMOVETO(arr,x,y){
+	return arr.concat([FLOAT,x,FLOAT,y,MOVETO]);
+}
+function instSTROKESTYLE(arr,r,g,b,a){
+	return arr.concat([INTEGER, r, INTEGER, g, INTEGER, b, FLOAT, a, STROKESTYLE]);
+}
+function instFILLSTYLE(arr,r,g,b,a){
+	return arr.concat([INTEGER, r, INTEGER, g, INTEGER, b, FLOAT, a, FILLSTYLE]);
+}
+/*********Testing**************/
+
+let test_code = [
+	FLOAT, 10.0, //[10.0]
+	LINEWIDTH,   //[]
+	INTEGER, 255,//[255]
+	INTEGER, 0,  //[255,0]
+	INTEGER, 0,  //[255,0,0]
+	FLOAT, 0.5,  //[255,0,0,0.5]
+	STROKESTYLE,   //[]
+	BEGINPATH,  // []        Begin Path, no stack change
+	INTEGER,100,// [100]     Add the x value to the stack
+	INTEGER,200,// [100,200] Add the y value to the stack
+	MOVETO,     // []        Consume the x, y values above
+	INTEGER, 0, // [0]
+	INTEGER, 0, // [0,0]
+	LINETO,     // []
+	STROKE      // End Drawing
+	];
+
+let test_code2 = [];
+test_code2=instSTROKESTYLE(test_code2,0,255,0,1);
+test_code2=instLINEWIDTH(test_code2,5);
+test_code2=instBEGINPATH(test_code2);
+test_code2=instMOVETO(test_code2,0,10);
+test_code2=instLINETO(test_code2,500,10);
+test_code2=instSTROKE(test_code2);
+console.log(test_code2);
+
+
+class VirtualMachine{
+	constructor(ctx){
+		debug("Creating Virtual Painting Machine")
+		this.ctx = ctx;
+		this.stack = [];
+		this.max_stack_size = 255;
+	}
+
+	pop(){
+		return this.stack.pop();
+	}
+	push(val){
+		this.stack.push(val);
+		if(this.stack.length>this.max_stack_size)
+			throw new Error("Exceeded VM max stack!");
+	}
+
+	interpret(bytecode){
+		let i = 0;
+		let x,y,r,g,b,a;
+		while(i<bytecode.length){
+			let instruction = bytecode[i];
+			switch(instruction){
+				case STRING:
+				case INTEGER:
+				case FLOAT:
+					this.push(bytecode[++i]);
+					debug(`Pushed ${bytecode[i]} onto the stack`);
+					break;	
+				case LINEJOIN:
+					x=this.pop();
+					this.ctx.lineJoin=x;
+					debug(`Line join set to ${x}`);
+					break;
+				case LINECAP:
+					x=this.pop();
+					this.ctx.lineCap=x;
+					debug(`Line cap set to ${x}`);
+					break;
+				case LINEWIDTH:
+					x=this.pop();
+					this.ctx.lineWidth=x;
+					debug(`Line width set to ${x}`);
+					break;
+				case STROKESTYLE:
+					a=this.pop();
+					b=this.pop();
+					g=this.pop();
+					r=this.pop();
+					this.ctx.strokeStyle=`rgba(${r},${g},${b},${a})`;
+					debug(`Set Stroke Style To rgba(${r},${g},${b},${a})`);
+					break;
+				case FILLSTYLE:
+					a=this.pop();
+					b=this.pop();
+					g=this.pop();
+					r=this.pop();
+					this.ctx.fillStyle=`rgba(${r},${g},${b},${a})`;
+					break;
+				
+				case STROKE:
+					this.ctx.stroke();
+					debug(`Stroke path`);
+					break;
+				case FILL:
+					this.ctx.fill();
+					debug(`Fill path`);
+					break;
+				case BEGINPATH:
+					this.ctx.beginPath();
+					debug(`Begin Path`)
+					break;
+				case LINETO:
+					y = this.pop();
+					x = this.pop();
+					this.ctx.lineTo(x,y);
+					debug(`Line to (${x},${y})`);
+					break;
+				case MOVETO:
+					y = this.pop();
+					x = this.pop();
+					this.ctx.moveTo(x,y);
+					debug(`Move To (${x},${y})`);
+					break;
+				case CLOSEPATH:
+					this.ctx.closePath();
+					debug(`Close Path`);
+					break;
+				default:
+					break;
+			}
+			i++;
+		}
+	}
+}
+
 class JSPaint{
 	constructor(canvasID){
 		this.canvas= document.getElementById(canvasID);
 		this.ctx = this.canvas.getContext('2d');
 		this.isPainting=false;
-		this.lineWidth=3;
-		this.rgba = 'rgba(100,0,0,1)';
+		
+		this.line_parameters={
+			lineWidth:3.0,
+			lineCap:'round', //butt, square,
+			lineJoin:'round',//bevel, miter,
+			//miterLimit:10,
+			//lineDashOffset:0
+		}
+
+		this.styles={
+			strokeStyle:'rgba(0,0,0,1)',
+			fillStyle:'rgba(0,0,0,1)'
+		}
+
 		this.x = 0;
 		this.y = 0;
-
+		this.vm = new VirtualMachine(this.ctx);
+		this.vm.interpret(test_code);
+		this.vm.interpret(test_code2);
+		this.drawPath=true;
 		this.canvas.addEventListener("pointermove",this.penPosition.bind(this));
 		this.canvas.addEventListener("pointerdown",this.startPainting.bind(this));
 		this.canvas.addEventListener("pointerup",this.stopPainting.bind(this));
 
-		this.pen = chalkCanvas(this.lineWidth,this.rgba);
 		}
 
 	set lineWidth(val){
@@ -33,6 +263,13 @@ class JSPaint{
 				break;
 			case "-":
 				this.lineWidth-=1;
+				break;
+			case " ":
+				this.canvas.width=this.canvas.width;
+				break;
+			default:
+				break;
+
 		}
 	}
 
@@ -42,13 +279,9 @@ class JSPaint{
 	}
 
 	startPainting(e){
-		this.pen = chalkCanvas(this.lineWidth,this.rgba);
 		this.isPainting=true;
-		
-		if(this.drawPath){
-			this.ctx.beginPath();
-			this.ctx.moveTo(this.x,this.y);
-		}
+		this.ctx.beginPath();
+		this.ctx.moveTo(this.x,this.y);
 	}
 
 	stopPainting(e){
@@ -61,47 +294,19 @@ class JSPaint{
 	}
 
 	draw(){
-		if(this.drawPath){
-			this.ctx.lineWidth=this.lineWidth;
-			this.ctx.lineCap='round';
-			this.ctx.lineTo(this.x,this.y);
-			this.ctx.stroke();
-		}else{
-			this.ctx.drawImage(this.pen,this.x,this.y);
-		}
+		this.ctx.lineWidth=this.lineWidth;
+		this.ctx.lineCap='round';
+		this.ctx.lineTo(this.x,this.y);
+		this.ctx.stroke();
 	}
 }
-
-function chalkCanvas(size,rgba='rgba(0,0,0,1)'){
-	const patternCanvas = document.createElement('canvas');
-	const patternContext = patternCanvas.getContext('2d');
-
-	patternCanvas.width=Math.floor(size*1.05);
-	patternCanvas.height=Math.floor(size*1.05);
-
-	patternContext.fillStyle='rgba(255,255,255,0)';
-	patternContext.fillRect(0,0,patternCanvas.width,patternCanvas.height);
-	patternContext.fillStyle=rgba;
-
-	const ss = size*size;
-	const s2 = size/2.0;
-	for(let i = 0;i<size;i++){
-		for(let j = 0;j<size;j++){
-			if(Math.pow(i-s2,2)+Math.pow(j-s2,2)<=ss/4)
-				if(Math.random()>0.45)
-					patternContext.fillRect(i,j,1,1);
-		}
-	}
-	patternContext.stroke();
-	return patternCanvas;
-
-}
-
 
 function main(canvasID){
+	debug("Creating Paint Application...");
 	const paintApp = new JSPaint(canvasID);
 	
 	window.addEventListener("keyup",paintApp.handleKeyEvent.bind(paintApp));
+	
 	function JSPaintLoop(){
 		paintApp.update();
 		requestAnimationFrame(JSPaintLoop);
